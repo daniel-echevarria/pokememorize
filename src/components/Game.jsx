95d306de6309606907d9422e01ab9dc91a1c67e4
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-08-19 16:39:56 +0200

    Implement shuffling of the cards after click

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,15 +1,18 @@
 import Card from "./Card";
 import "../styles/Game.css";
 import pokemonNames from "../data/pokemonNames";
-import { useEffect } from "react";
 import { useState } from "react";
+import _ from "lodash";
 
 const Game = () => {
+  const [pokeOptions, setPokeOptions] = useState(pokemonNames);
+
   const handleClick = (e) => {
+    setPokeOptions(_.shuffle(pokeOptions));
     console.log(e.target.id);
   };
 
-  const pokemonList = pokemonNames.map((name) => (
+  const pokemonList = pokeOptions.map((name) => (
     <Card key={name} pokemonName={name} handleClick={handleClick} />
   ));
 
