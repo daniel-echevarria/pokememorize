9721ff04e62759b3b0c9b929f51646dafafcaaf7
Kasper Bak <dan.echevarri@gmail.com>
2024-12-17 10:55:46 +0100

    Write levels in their own file

---
+++ src/data/levels.js
@@ -0,0 +1,10 @@
+const levels = [
+  { id: 1, text: "Extra Easy", numCards: 4 },
+  { id: 2, text: "Easy", numCards: 8 },
+  { id: 3, text: "Moderate", numCards: 12 },
+  { id: 4, text: "Hard", numCards: 16 },
+  { id: 5, text: "Extra Hard", numCards: 24, last: true },
+  { id: 6, text: "Impossible", numCards: 120, hidden: true },
+];
+
+export default levels;
